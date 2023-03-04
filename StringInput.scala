4b5077f10acb2a645fac6fd931e97ee307f7632a
ajit1865 <ajit.kumar@knoldus.com>
2023-03-04 10:00:51 +0530

    Product of array elements

--- StringInput.scala
+++ StringInput.scala
@@ -1,3 +1,7 @@
 object StringInput {
+  def main(args: Array[String]): Unit = {
+    val name = scala.io.StdIn.readLine("What is your name")
+    println("Your name is "+name)
+  }
 
 }
---
+++ target/ArrayProduct.scala
@@ -0,0 +1,10 @@
+package target
+
+object ArrayProduct extends App{
+  //Creating variable array to store array values
+  val array = Array(1,2,3,4,5)
+    //Multipliying all elements by product
+  val product= array.product
+  println("Product of all elements are : "+product)
+
+}
