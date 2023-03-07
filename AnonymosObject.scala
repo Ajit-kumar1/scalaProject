fa0a66ed7cf2013f618a2451967e945f83b83d74
ajit1865 <ajit.kumar@knoldus.com>
2023-03-07 17:38:44 +0530

    first commit

---
+++ AnonymosObject.scala
@@ -0,0 +1,6 @@
+object AnonymosObject extends App{
+//  val doubler: (Int, Int) => String = (value: Int, secondValue: Int)=> value * secondValue + "asasd"
+//  println(doubler(4,5))
+val number = (firstNum: Int, secondNum: Int) => firstNum * secondNum+"abc"
+  println(number(5,6))
+}
