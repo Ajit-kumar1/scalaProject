dd8950ca89d7350701ec80a5a7b5a405efd52f1b
ajit1865 <ajit.kumar@knoldus.com>
2023-03-10 12:18:22 +0530

    wip

--- UserInput.scala
+++ UserInput.scala
@@ -7,6 +7,7 @@ object UserInput {
     val name = scala.io.StdIn.readLine("What is your name")
     print("What is your name")
     val age = scala.io.StdIn.readInt()
+
     val gender = scala.io.StdIn.readLine("What is your name")
     (name, age, gender)
   }
