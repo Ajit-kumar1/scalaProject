2c1a0933d13ca5a69f0bee70d69700646d01eb05
ajit1865 <ajit.kumar@knoldus.com>
2023-03-10 12:07:49 +0530

    Taking input from input method

---
+++ UserInput.scala
@@ -0,0 +1,13 @@
+object UserInput {
+  def main(args: Array[String]):Unit ={
+    val (name, age , gender)= input()
+    println(s"$name  $age $gender")
+  }
+  def input(): (String , Int, String)={
+    val name = scala.io.StdIn.readLine("What is your name")
+    print("What is your name")
+    val age = scala.io.StdIn.readInt()
+    val gender = scala.io.StdIn.readLine("What is your name")
+    (name, age, gender)
+  }
+}
